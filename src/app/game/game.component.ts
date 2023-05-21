6dc6a8df4279649d1de98e337f5dbddce262bc80
lukechant <luke_chant@yahoo.co.uk>
2023-05-22 00:12:50 +0100

    first commit

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,150 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+enum Direction {
+  Up = 'up',
+  Down = 'down',
+  Left = 'left',
+  Right = 'right',
+}
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  gridSize = 20;
+  gridSizePixels = 20;
+  canvasSize = 400;
+  snake: Point[] = [];
+  direction!: Direction | string;
+  food!: Point;
+  interval: any;
+  score: number = 0;
+
+  ngOnInit() {
+    this.startGame();
+  }
+
+  startGame() {
+    this.snake = [
+      { x: Math.floor(this.gridSize / 2), y: Math.floor(this.gridSize / 2) },
+      { x: Math.floor(this.gridSize / 2), y: Math.floor(this.gridSize / 2) + 1 },
+      { x: Math.floor(this.gridSize / 2), y: Math.floor(this.gridSize / 2) + 2 }
+    ];
+    this.direction = Direction.Right;
+    this.food = this.generateFood();
+    this.score = 0;
+  
+    this.interval = setInterval(() => {
+      this.move();
+    }, 200);
+  }
+
+  move() {
+    const head = { ...this.snake[0] };
+    switch (this.direction) {
+      case Direction.Up:
+        head.y -= 1;
+        break;
+      case Direction.Down:
+        head.y += 1;
+        break;
+      case Direction.Left:
+        head.x -= 1;
+        break;
+      case Direction.Right:
+        head.x += 1;
+        break;
+    }
+
+    this.snake.unshift(head);
+
+    if (this.isCollisionWithFood()) {
+      this.score++;
+      this.food = this.generateFood();
+    } else {
+      this.snake.pop();
+    }
+
+    if (this.isCollisionWithWalls() || this.isCollisionWithSelf()) {
+      clearInterval(this.interval);
+      alert('Game Over!');
+    }
+  }
+
+  isCollisionWithFood() {
+    const head = this.snake[0];
+    return head.x === this.food.x && head.y === this.food.y;
+  }
+
+  isCollisionWithWalls() {
+    const head = this.snake[0];
+    return (
+      head.x < 0 ||
+      head.y < 0 ||
+      head.x >= this.gridSize ||
+      head.y >= this.gridSize
+    );
+  }
+
+  isCollisionWithSelf() {
+    const head = this.snake[0];
+    for (let i = 1; i < this.snake.length; i++) {
+      if (head.x === this.snake[i].x && head.y === this.snake[i].y) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  generateFood(): Point {
+    let newFood: Point;
+    do {
+      newFood = {
+        x: Math.floor(Math.random() * this.gridSize),
+        y: Math.floor(Math.random() * this.gridSize)
+      };
+    } while (this.isFoodOnSnake(newFood));
+    return newFood;
+  }
+
+  isFoodOnSnake(food: Point) {
+    for (let i = 0; i < this.snake.length; i++) {
+      if (this.snake[i].x === food.x && this.snake[i].y === food.y) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    const { key } = event;
+    const validKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+
+    if (validKeys.includes(key)) {
+      event.preventDefault();
+      this.changeDirection(key.substr(5).toLowerCase());
+    }
+  }
+
+  changeDirection(newDirection: string) {
+    const direction = newDirection.toLowerCase() as Direction;
+    if (
+      (this.direction === Direction.Up && direction === Direction.Down) ||
+      (this.direction === Direction.Down && direction === Direction.Up) ||
+      (this.direction === Direction.Left && direction === Direction.Right) ||
+      (this.direction === Direction.Right && direction === Direction.Left)
+    ) {
+      return;
+    }
+  
+    this.direction = direction;
+  }
+}
